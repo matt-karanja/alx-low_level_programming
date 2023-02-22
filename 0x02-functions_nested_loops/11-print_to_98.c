ef4188a38618a32d2a99baa2aaacce49653ef79b
matt-karanja <matthew.n.karanja@gmail.com>
2023-02-22 14:18:10 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,13 +20,13 @@ void print_to_98(int n)
 				printf("%d\n", i);
 		}
 	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
 		{
-			for (j = n; j >= 98; j--)
-			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
-			}
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
+	}
 }
